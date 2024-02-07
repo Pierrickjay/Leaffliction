db08dbcbe5798c13d9c33d6b109b9659db4db7c2
Maxence BOCQUEL <maxence.bocquel@gmail.com>
2024-02-07 12:00:23 +0100

    commit avant merge

--- src/Train.py
+++ src/Train.py
@@ -7,6 +7,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 import os
 import argparse
+import zipfile
 
 
 def loadDataset(path, img_size):
@@ -27,14 +28,16 @@ def processImgDataSet(path):
          if len(filenames)]
     img_path_list = np.array([element for sous_liste in
                               img_path_list for element in sous_liste])
+    list_path_long = list(set([img[2] for img in img_path_list]))
+    img_path_list = [[img[0], img[1], img[2].replace(os.path.commonpath(list_path_long) + '/', '') ] for img in img_path_list]
     img_array = np.array(
          [np.array(Image.open(str(img_path[0] + "/" + img_path[1]), "r"))
           for img_path in img_path_list])
     img_back_removed = [removeBack(img, 5000, 1, 10) for img in img_array]
     img_back_removed_IMG = [Image.fromarray(img_array)
                             for img_array in img_back_removed]
-    [os.makedirs("increased/" + path[2], exist_ok=True)
-     for path in img_path_list]
+    [os.makedirs("increased/" + path, exist_ok=True)
+     for path in list(set([img[2] for img in img_path_list]))]
     [img.save(os.path.join(
         "increased", path[2], path[1].split(".")[0] + ".png"), format="PNG")
      for path, img in zip(img_path_list, img_back_removed_IMG)]
@@ -114,7 +117,7 @@ def main(**kwargs):
 
         # Datasets
         print("Loading dataset\n")
-        dataset = loadDataset("increased/leaves/images", img_size)
+        dataset = loadDataset("increased", img_size)
         train_ds, validation_ds = get_dataset_partition_tf(dataset)
         train_ds = train_ds.cache().shuffle(1000).prefetch(
             buffer_size=tf.data.AUTOTUNE)
